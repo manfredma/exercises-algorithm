191314ee602b6affa3ff6bdff4527aec6b6f9533
maxingfang <maxingfang@meituan.com>
2026-06-11 17:36:42 +0800

    feat: p0236 二叉树的最近公共祖先，Solution 路径对比法 + Solution2 后序递归单次 DFS

--- leet-code/src/main/java/manfred/exercises/leetcode/p0236/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0236/Solution.java
@@ -1,7 +1,47 @@
 package manfred.exercises.leetcode.p0236;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        return null;
+        List<TreeNode> root2p = new ArrayList<>();
+        List<TreeNode> root2q = new ArrayList<>();
+        construct(root, p, q, root2p, root2q);
+        TreeNode lowestCommonAncestor = root;
+        for (int i = 1; i < Math.min(root2p.size(), root2q.size()); i++) {
+            if (root2p.get(i) == root2q.get(i)) {
+                lowestCommonAncestor = root2p.get(i);
+            }
+        }
+        return lowestCommonAncestor;
+    }
+
+    private void construct(TreeNode node, TreeNode p, TreeNode q, List<TreeNode> root2p, List<TreeNode> root2q) {
+        if (node == null || isFinished(root2p, p) && isFinished(root2q, q)) {
+            return;
+        }
+        // 只要找到了p 或 q，则从根节点到 p或q 的路径节点就不再变化
+        if (!isFinished(root2p, p)) {
+            root2p.add(node);
+        }
+        if (!isFinished(root2q, q)) {
+            root2q.add(node);
+        }
+        if (!isFinished(root2p, p) || !isFinished(root2q, q)) {
+            construct(node.left, p, q, root2p, root2q);
+            construct(node.right, p, q, root2p, root2q);
+        }
+        // 清理自己
+        if (!isFinished(root2p, p)) {
+            root2p.remove(node);
+        }
+        if (!isFinished(root2q, q)) {
+            root2q.remove(node);
+        }
+    }
+
+    private boolean isFinished(List<TreeNode> root2p, TreeNode p) {
+        return !root2p.isEmpty() && root2p.get(root2p.size() - 1) == p;
     }
 }
---
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0236/Solution2.java
@@ -0,0 +1,20 @@
+package manfred.exercises.leetcode.p0236;
+
+class Solution2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
