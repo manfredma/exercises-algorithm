f43eb30019e1895dd3d6a8f7cb2a54344034d4d5
maxingfang <maxingfang@meituan.com>
2026-06-11 16:01:51 +0800

    feat: p0543 二叉树的直径，递归分治求每个节点的左右高度

--- leet-code/src/main/java/manfred/exercises/leetcode/p0543/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0543/Solution.java
@@ -2,6 +2,30 @@ package manfred.exercises.leetcode.p0543;
 
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        return 0;
+        return diameterNodeOfBinaryTree(root) -1;
+
+    }
+
+    private int diameterNodeOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = highestOfBinaryTree(root.left);
+        int right = highestOfBinaryTree(root.right);
+        int diameterRoot = left + right + 1;
+
+        int diameterLeft = diameterNodeOfBinaryTree(root.left);
+        int diameterRight = diameterNodeOfBinaryTree(root.right);
+
+        return Math.max(Math.max(diameterLeft, diameterRight), diameterRoot);
+    }
+
+    private int highestOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int highestLeft = highestOfBinaryTree(root.left);
+        int highestRight = highestOfBinaryTree(root.right);
+        return Math.max(highestLeft, highestRight) + 1;
     }
 }
