a5c94e39163c8f2ef34a1752b987c9827ac275ee
maxingfang <maxingfang@meituan.com>
2026-06-26 14:25:02 +0800

    feat: 完成第 682 题「棒球比赛」

--- leet-code/src/main/java/manfred/exercises/leetcode/p0682/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0682/Solution.java
@@ -1,7 +1,34 @@
 package manfred.exercises.leetcode.p0682;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 class Solution {
     public int calPoints(String[] operations) {
-        return 0; // TODO: 待实现
+        Deque<Integer> stack = new ArrayDeque<>();
+        for (String operation : operations) {
+            switch (operation) {
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    stack.push(stack.peek() * 2);
+                    break;
+                case "+":
+                    int pre = stack.pop();
+                    int cur = stack.peek() + pre;
+                    stack.push(pre);
+                    stack.push(cur);
+                    break;
+                default:
+                    stack.push(Integer.valueOf(operation));
+                    break;
+            }
+        }
+        int result = 0;
+        while (!stack.isEmpty()) {
+            result += stack.pop();
+        }
+        return result;
     }
 }
