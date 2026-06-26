852c83792675c293cc0ec830df436badcf879b46
maxingfang <maxingfang@meituan.com>
2026-06-26 14:42:19 +0800

    feat: 完成第 605 题「种花问题」

--- leet-code/src/main/java/manfred/exercises/leetcode/p0605/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0605/Solution.java
@@ -2,6 +2,17 @@ package manfred.exercises.leetcode.p0605;
 
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        return false; // TODO: 待实现
+        int canPlace = 0;
+        int pre = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            int cur = flowerbed[i];
+            if (cur == 0 && pre == 0 && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                canPlace++;
+                pre = 1;
+            } else {
+                pre = cur;
+            }
+        }
+        return canPlace >= n;
     }
 }
