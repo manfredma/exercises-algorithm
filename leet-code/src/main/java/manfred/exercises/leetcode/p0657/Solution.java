239f4daaca792168b5607716e6f538d38647c9fa
maxingfang <maxingfang@meituan.com>
2026-06-26 16:17:31 +0800

    feat: 完成第 657 题「机器人能否返回原点」

--- leet-code/src/main/java/manfred/exercises/leetcode/p0657/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0657/Solution.java
@@ -2,6 +2,23 @@ package manfred.exercises.leetcode.p0657;
 
 class Solution {
     public boolean judgeCircle(String moves) {
-        return false; // TODO: 待实现
+        int x = 0, y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            switch (moves.charAt(i)) {
+                case 'R':
+                    x++;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'U':
+                    y++;
+                    break;
+                default:
+                    y--;
+            }
+
+        }
+        return x == 0 && y == 0;
     }
 }
