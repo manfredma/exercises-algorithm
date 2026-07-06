bfaa04bbda6c03f7c8dcb11e0d08e8d9c4c3fb70
maxingfang <maxingfang@meituan.com>
2026-07-06 14:05:53 +0800

    feat: 实现第 1523 题「在区间范围内统计奇数数目」解法

--- leet-code/src/main/java/manfred/exercises/leetcode/p1523/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p1523/Solution.java
@@ -2,6 +2,6 @@ package manfred.exercises.leetcode.p1523;
 
 class Solution {
     public int countOdds(int low, int high) {
-        return 0;   // TODO: 待实现
+        return (high % 2 == 0) ? (high - low + 1) / 2 : (high - low) / 2 + 1;
     }
 }
