d2e82cb7182290cca7e6bac8d373f6b9c35bfbb2
maxingfang <maxingfang@meituan.com>
2026-07-07 18:50:26 +0800

    feat: 实现第 345 题「反转字符串中的元音字母」栈与双指针解法

--- leet-code/src/main/java/manfred/exercises/leetcode/p0345/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0345/Solution.java
@@ -1,7 +1,25 @@
 package manfred.exercises.leetcode.p0345;
 
+import java.util.*;
+
 class Solution {
     public String reverseVowels(String s) {
-        return "";   // TODO: 待实现
+        Set<Character> vowels = new HashSet<>(Arrays.asList('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u'));
+
+        Deque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.contains(s.charAt(i))) {
+                sb.append(stack.pop());
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
     }
 }
---
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0345/Solution2.java
@@ -0,0 +1,30 @@
+package manfred.exercises.leetcode.p0345;
+
+class Solution2 {
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+        int l = 0, r = chars.length - 1;
+        while (l < r) {
+            while (l < r && !isVowel(chars[l])) l++;
+            while (l < r && !isVowel(chars[r])) r--;
+            if (l < r) {
+                char tmp = chars[l];
+                chars[l] = chars[r];
+                chars[r] = tmp;
+                l++;
+                r--;
+            }
+        }
+        return new String(chars);
+    }
+
+    private boolean isVowel(char c) {
+        switch (c) {
+            case 'a': case 'e': case 'i': case 'o': case 'u':
+            case 'A': case 'E': case 'I': case 'O': case 'U':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
