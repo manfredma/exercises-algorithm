0db398b671380bb97ffc935cf5a5d7d2840cd651
maxingfang <maxingfang@meituan.com>
2026-07-29 11:27:28 +0800

    feat: add leetcode 342

--- leet-code/src/main/java/manfred/exercises/leetcode/p0300/Solution2.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0300/Solution2.java
@@ -6,7 +6,7 @@ package manfred.exercises.leetcode.p0300;
 class Solution2 {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
-        int result = 1;
+        int result = 0;
         for (int i = 0; i < nums.length; i++) {
             dp[i] = 1;
             for (int j = 0; j < i; j++) {
