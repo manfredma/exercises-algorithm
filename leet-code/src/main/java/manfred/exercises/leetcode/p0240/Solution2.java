698f722f9560a97bf2f4692ef7c5ca038e0be3db
maxingfang <maxingfang@meituan.com>
2026-07-29 19:33:30 +0800

    feat: add leetcode 240

---
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0240/Solution2.java
@@ -0,0 +1,21 @@
+package manfred.exercises.leetcode.p0240;
+
+/** 从左下角开始，每次排除整行或整列。 */
+class Solution2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length - 1;
+        int column = 0;
+        while (row >= 0 && column < matrix[0].length) {
+            int current = matrix[row][column];
+            if (current == target) {
+                return true;
+            }
+            if (current > target) {
+                row--;
+            } else {
+                column++;
+            }
+        }
+        return false;
+    }
+}
