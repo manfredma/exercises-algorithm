2b2b826d2d120b0f8ee81a2d964c2d4c60127c2d
maxingfang <maxingfang@meituan.com>
2026-07-30 13:41:59 +0800

    feat: add LeetCode exercises 367, 441 and 797

---
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0797/Solution.java
@@ -0,0 +1,9 @@
+package manfred.exercises.leetcode.p0797;
+
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        return null; // TODO: 待实现
+    }
+}
