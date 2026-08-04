1e8c0fcc0536b60c5310ad28cc91ed6b0b449438
maxingfang <maxingfang@meituan.com>
2026-08-04 12:13:46 +0800

    feat(leetcode): add solutions for problem 797

--- leet-code/src/main/java/manfred/exercises/leetcode/p0797/Solution.java
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0797/Solution.java
@@ -1,9 +1,30 @@
 package manfred.exercises.leetcode.p0797;
 
+import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        return null; // TODO: 待实现
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> curPath = new ArrayList<>();
+        findAllPath(graph, 0, graph.length - 1, result, curPath);
+        return result;
+    }
+
+    private void findAllPath(int[][] graph, int from, int target, List<List<Integer>> paths, List<Integer> curPath) {
+        curPath.add(from);
+        if (from == target) {
+            // 说明已经查询到，则记录下来
+            paths.add(new ArrayList<>(curPath));
+            // 移除当前元素
+            curPath.remove(curPath.size() - 1);
+            return;
+        }
+        int[] nexts = graph[from];
+        for (int next : nexts) {
+            findAllPath(graph, next, target, paths, curPath);
+        }
+        // 移除当前路径
+        curPath.remove(curPath.size() - 1);
     }
 }
---
+++ leet-code/src/main/java/manfred/exercises/leetcode/p0797/Solution2.java
@@ -0,0 +1,45 @@
+package manfred.exercises.leetcode.p0797;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+class Solution2 {
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<List<Integer>> result = new ArrayList<>();
+        Deque<Integer> nodes = new ArrayDeque<>();
+        Deque<Integer> nextIndexes = new ArrayDeque<>();
+        List<Integer> path = new ArrayList<>();
+
+        nodes.push(0);
+        nextIndexes.push(0);
+        path.add(0);
+
+        int target = graph.length - 1;
+        while (!nodes.isEmpty()) {
+            int node = nodes.peek();
+            if (node == target) {
+                result.add(new ArrayList<>(path));
+                nodes.pop();
+                nextIndexes.pop();
+                path.remove(path.size() - 1);
+                continue;
+            }
+
+            int nextIndex = nextIndexes.pop();
+            if (nextIndex == graph[node].length) {
+                nodes.pop();
+                path.remove(path.size() - 1);
+                continue;
+            }
+
+            nextIndexes.push(nextIndex + 1);
+            int next = graph[node][nextIndex];
+            nodes.push(next);
+            nextIndexes.push(0);
+            path.add(next);
+        }
+        return result;
+    }
+}
